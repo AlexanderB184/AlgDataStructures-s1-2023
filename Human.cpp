163403721237eda0388e8f1183c682e124be6f29
AlexanderB184 <a1849988@adelaide.edu.au>
2023-03-10 15:28:18 +1030

    TRY NOW

--- Human.cpp
+++ Human.cpp
@@ -12,11 +12,9 @@ Human::Human() { name = "Human"; }
 Human::Human(string _name) { name = _name; }
 
 Move* Human::makeMove() {
-  bool isValidMove = false;
   std::string move;
   cout << "Choose a move:" << endl;
   // ask for move
   cin >> move;
-
   return MoveConstructor::stringToMove(move);
 }
