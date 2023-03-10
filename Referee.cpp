f3c292ec2dfcfe192a48eb92b2af59362214cdf0
AlexanderB184 <a1849988@adelaide.edu.au>
2023-03-10 15:55:10 +1030

    NOW!

--- Referee.cpp
+++ Referee.cpp
@@ -3,12 +3,15 @@
 #include "Move.h"
 
 #include <string>
-
+#include <iostream>
 
 Player * Referee::refGame(Player * Player1, Player * Player2) {
   //Make the moves
   Move * Player1Move = Player1->makeMove();
   Move* Player2Move = Player2->makeMove();
+
+  std::cout << "Player 1: " << Player1->getName() << "\t" << Player1Move->getName() << std::endl;
+  std::cout << "Player 2: " << Player2->getName() << "\t" << Player2Move->getName() << std::endl;
   // Player 1 wins
   if (Player1Move->beats(Player2Move)) {
     return Player1;
