823a811eede63ac320097717a0a711b504d2c13a
AlexanderB184 <a1849988@adelaide.edu.au>
2023-03-16 11:55:06 +1030

    Prac3

--- EfficientTruckloads.cpp
+++ EfficientTruckloads.cpp
@@ -1,25 +1,25 @@
 #include "EfficientTruckloads.h"
 
 int EfficientTruckloads::numTrucks(int numCrates, int loadSize) {
-  static std::map<int, int> nCrate;
-  std::map<int, int>::iterator it = nCrate.begin();
-  nCrate.find(numCrates);
-  if (it != nCrate.end()) {
-    nCrate.erase(it);
+  static std::map<int, int> truckMap;
+  std::map<int, int>::iterator it = truckMap.begin();
+  truckMap.find(numCrates);
+  if (it != truckMap.end()) {
+    truckMap.erase(it);
     if (numCrates == 0) {
-      nCrate[0] = 0;
+      truckMap[0] = 0;
       return 0;
     }
     if (numCrates <= loadSize) {
-      nCrate[numCrates] = 1;
+      truckMap[numCrates] = 1;
       return 1;
     }
     int halfCrates = numCrates >> 1;
     int otherHalfCrates = numCrates - halfCrates;
     int output =
         numTrucks(halfCrates, loadSize) + numTrucks(otherHalfCrates, loadSize);
-    nCrate[numCrates] = output;
+    truckMap[numCrates] = output;
     return output;
   }
-  return nCrate[numCrates];
+  return truckMap[numCrates];
 }
