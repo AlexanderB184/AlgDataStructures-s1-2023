c2f49053a341e40e45af5ad5c5424bea3a46590d
AlexanderB184 <a1849988@adelaide.edu.au>
2023-04-05 15:41:31 +0930

    attempt 2

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
 int main(void) {
   std::vector<int> list;
   int num;
-  while (std::cin >> num){
+  while (std::cin >> num) {
     list.push_back(num);
   }
 
@@ -15,8 +15,13 @@ int main(void) {
   list = B.sort(list);
   RecursiveBinarySearch R;
   bool found = R.search(list, 1);
+  if (found) {
+    std::cout << "true";
+  }
+  else {
+    std::cout << "false";
+  }
 
-  std::cout << std::endl << found;
   for (size_t i = 0; i < list.size(); i++) std::cout << " " << list.at(i);
   std::cout << std::endl;
 
