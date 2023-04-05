b8cca97a7493b8b7c912aa73024b16e09516fcd3
AlexanderB184 <a1849988@adelaide.edu.au>
2023-04-05 15:39:30 +0930

    fixxed?

--- main.cpp
+++ main.cpp
@@ -6,22 +6,10 @@
 
 int main(void) {
   std::vector<int> list;
-  list.push_back(5);
-  list.push_back(2);
-  list.push_back(3);
-  list.push_back(2);
-  list.push_back(5);
-  list.push_back(9);
-  list.push_back(7);
-  list.push_back(1);
-   list.push_back(3);
-  list.push_back(6);
-  list.push_back(8);
-  list.push_back(9);
-  list.push_back(1);
-  list.push_back(2);
-  list.push_back(4);
-  list.push_back(16);
+  int num;
+  while (std::cin >> num){
+    list.push_back(num);
+  }
 
   QuickSort B;
   list = B.sort(list);
