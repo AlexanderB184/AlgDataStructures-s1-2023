6567bf088053bac9eeb43c8cbb8d7126af3c22ef
AlexanderB184 <a1849988@adelaide.edu.au>
2023-04-27 11:53:41 +0930

    BIGNUM

---
+++ BigNumCalc.cpp
@@ -0,0 +1,7 @@
+#include "BigNumCalc.h"
+
+std::list<int> bigNumCalc::buildBigNum(std::string numString) {
+  for (int i = 0; i < numString.length(); i++) {
+    
+  }
+}
---
+++ BigNumCalc.h
@@ -0,0 +1,14 @@
+#ifndef BIG_NUM_CALC_H
+#define BIG_NUM_CALC_H
+
+#include <list>
+#include <string>
+
+class bigNumCalc {
+  std::list<int> buildBigNum(std::string numString);
+  std::list<int> add(std::list<int> num1, std::list<int> num2);
+  std::list<int> sub(std::list<int> num1, std::list<int> num2);
+  std::list<int> mul(std::list<int> num1, std::list<int> num2);
+};
+
+#endif
